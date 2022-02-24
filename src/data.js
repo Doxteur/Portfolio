da71b1b601dd05cc39e7a2fd49e81979dcd79c8f
Doxteur <doxteurn@gmail.com>
2022-02-24 18:37:22 +0100

    First Commit

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const projects = [
+    {
+      title: "Breaking Bad",
+      subtitle: "Développement d'un site internet static sur le thème Breaking Bad",
+      description:
+        "Ce site a été réalisé lors de ma formation BTS et a été réalisé sur une période de 1 mois",
+      image: "https://cdn.dribbble.com/users/2182473/screenshots/11905465/media/399f5c4faae1996e4b2ffa54463610e0.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.goodcore.co.uk/blog/wp-content/webp-express/webp-images/uploads/2019/08/coding-vs-programming-2.jpg.webp",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.goodcore.co.uk/blog/wp-content/webp-express/webp-images/uploads/2019/08/coding-vs-programming-2.jpg.webp",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Todo List",
+      subtitle: "Application Web Réalisé en laravel",
+      description:
+        "Application réalisé en laravel, avec une base de données MySQL. Le but est de pouvoir gerer des tâches et s'organiser.",
+      image: "https://www.goodcore.co.uk/blog/wp-content/webp-express/webp-images/uploads/2019/08/coding-vs-programming-2.jpg.webp",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  export const skills = [
+    {
+      name: "HTML/CSS",
+    },
+    {
+      name: "JS/JQUERY",
+
+    },
+    {
+      name: "PHP/LARAVEL",
+
+    },
+    {
+      name: "UNITY/C#",
+
+    },
+  ];
+  export const testimonials = [
+    {
+      name: "Priscilla Moreno",
+      image: "https://randomuser.me/api/portraits/med/women/32.jpg",
+      compagny: "Google",
+      quote: "Jimmy est le meilleur programmeur avec qui j'ai travaillé. Il porte toujours une attention particulière aux détails et est extrêmement minutieux dans son travail."
+    },
+    {
+      name: "Gregory Gonzalez",
+      image: "https://randomuser.me/api/portraits/men/37.jpg",
+      compagny: "Google",
+      quote: "Programmeur tres rigoureux et attentif au demande du client. Je recommande vivement."
+    }, {
+      name: "Samuel Jacobs",
+      image: "https://randomuser.me/api/portraits/men/66.jpg",
+      compagny: "Google",
+      quote: "Je dirige une petite startup technologique et je devais trouver le meilleur programmeur pour mon prochain projet. Après avoir interviewé quelques personnes différentes, j'ai trouvé Jimmy et il était parfait pour le poste. Il a d'excellentes compétences techniques et est très fiable dans son travail. Il fait le travail dans les délais et ce fut un plaisir de travailler avec lui."
+    },
+    {
+      name: "Herbert Mitchell",
+      image: "https://randomuser.me/api/portraits/men/33.jpg",
+      compagny: "Google",
+      quote: "Jimmy est l'une des personnes les plus travailleuses que j'ai jamais rencontrées. Chaque fois qu'il est chargé d'un travail de programmation, il va au-delà pour s'assurer que les résultats sont impeccables. Il est une bouée de sauvetage absolue en ce qui concerne les délais."
+    },
+  ];
+        
